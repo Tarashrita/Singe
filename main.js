1d718d112a872b8590ccf581c357501ce5fad453
Tarashrita Ganguly <106442772+Tarashrita@users.noreply.github.com>
2023-10-10 19:22:12 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 song1="";
 song2="";
+rightWristX=0;
+rightWristY=0;
+leftWristX=0;
+leftWristY=0;
 
 function preload() {
     song1= loadSound("Harry_Potter_Theme.mp3");
@@ -11,6 +15,25 @@ function setup() {
     canvas.center();
     video = createCapture(VIDEO);
     video.hide();
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+    console.log("poseNet is Initialized!");
+}
+
+function gotPoses(results){
+    if(results.length > 0){
+        console.log(results);
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("rightWristX ="+rightWristX+" rightWristY ="+rightWristY);
+
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("leftWristX ="+leftWristX+" leftWristY ="+leftWristY);
+    }
 }
 
 function draw() {
@@ -18,5 +41,12 @@ function draw() {
 }
 
 function play() {
-    song.play();
+   if(leftWristX > rightWristX) {
+   song1.play();
+}
+if(rightWristX > leftWristX) {
+    song2.play()
+}
+song.setVolume(1);
+song.rate(1);  
 }
